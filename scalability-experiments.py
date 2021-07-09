46e2cbbd2f0fd109945e15903935ce9c806bf87f
Radu <radu.ciucanu@insa-cvl.fr>
2021-07-09 12:34:07 +0200

    end scenarion2

--- scalability-experiments.py
+++ scalability-experiments.py
@@ -27,7 +27,7 @@ for scenario in scenarios_experiment1:
 		for algo in algos:
 			print ("*" * 10 + " Scenario=", scenario, "N=", N, "algo=", algo)
 			output_file = DIR_EXP1_OUT + "scenario" + scenario + "_N_" + str(N) + "_" + algo + ".txt"
-			os.system("python3 " + algo + ".py " + str(nb_runs) + " " + str(N) + " " + input_file + " " + output_file + " 0")
+			#os.system("python3 " + algo + ".py " + str(nb_runs) + " " + str(N) + " " + input_file + " " + output_file + " 0")
 			R_list[algo], aggregate_time, aggregates_all[algo] = parse_json_output(output_file)
 			aggregates_time[algo].append(aggregate_time)		
 		# check that all algorithms give the same cumulative reward
